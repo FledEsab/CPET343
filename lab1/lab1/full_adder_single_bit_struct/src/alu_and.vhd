9fd6a0abb0bc55202f1c40895533c7e49147fff7
Ethan Saber <96797250+FledEsab@users.noreply.github.com>
2026-09-03 16:39:09 -0400

    final version

--- lab1/lab1/full_adder_single_bit_struct/src/alu_and.vhd
+++ lab1/lab1/full_adder_single_bit_struct/src/alu_and.vhd
@@ -4,21 +4,21 @@
     --09.03.26
     --and operation of two inputs
 ------------------------------------------------------
-    library ieee;
-    use ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
-    entity alu_and is
+entity alu_and is
     port(
 
-    a     :in std_logic;
-    b     :in std_logic;
-    c_out  :out std_logic;
+        a     :in std_logic;
+        b     :in std_logic;
+        c_out  :out std_logic;
 
     );
 
-    ARCHITECTURE beh of alu_and is
-    begin
+ARCHITECTURE beh of alu_and is
+begin
 
     c_out <= a and b;
 
-    end beh;
+end beh;
